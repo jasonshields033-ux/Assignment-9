a5a54fe8b66097507841f606f8805e1af0549d4f
jasonshields033-ux <jasonshields033@gmail.com>
2025-11-02 22:47:25 -0500

    social_network.py

--- social_network.py
+++ social_network.py
@@ -6,9 +6,14 @@ class Person:
         friends (list): A list of friends (Person objects).
     Methods:
         add_friend(friend): Adds a friend to the person's friend list.
-   '''
-    
-    pass # Delete this line to implement the Person class
+    '''
+    def __init__(self, name):
+        self.name = name
+        self.friends = []
+
+    def add_friend(self, friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
 
 
 class SocialNetwork:
@@ -21,9 +26,50 @@ class SocialNetwork:
         add_friendship(person1_name, person2_name): Creates a friendship between two people.
         print_network(): Prints the names of all people and their friends.
     '''
-    
-    pass # Delete this line to implement the SocialNetwork class
+    def __init__(self):
+        self.people = {}
 
+    def add_person(self, name):
+        if name in self.people:
+            print(f"{name} already exists in the network.")
+        else:
+            self.people[name] = Person(name)
+
+    def add_friendship(self, person1_name, person2_name):
+        if person1_name not in self.people or person2_name not in self.people:
+            print(f"Friendship not created. One or both people don't exist: {person1_name}, {person2_name}")
+        else:
+            person1 = self.people[person1_name]
+            person2 = self.people[person2_name]
+            person1.add_friend(person2)
+            person2.add_friend(person1)
+
+    def print_network(self):
+        for name, person in self.people.items():
+            friend_names = [friend.name for friend in person.friends]
+            print(f"{name} is friends with: {', '.join(friend_names)}")
 
 
 # Test your code here
+network = SocialNetwork()
+
+# Add people
+network.add_person("Alex")
+network.add_person("Jordan")
+network.add_person("Morgan")
+network.add_person("Taylor")
+network.add_person("Casey")
+network.add_person("Riley")
+
+# Create friendships
+network.add_friendship("Alex", "Jordan")
+network.add_friendship("Alex", "Morgan")
+network.add_friendship("Jordan", "Taylor")
+network.add_friendship("Jordan", "Johnny")  # Should print error
+network.add_friendship("Morgan", "Casey")
+network.add_friendship("Taylor", "Riley")
+network.add_friendship("Casey", "Riley")
+network.add_friendship("Morgan", "Riley")
+network.add_friendship("Alex", "Taylor")
+
+# Print the network
